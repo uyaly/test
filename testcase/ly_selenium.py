6580d08e49b5f60ec360823c10bd5fdb9955d001
uyaly <uyaly@qq.com>
2017-09-15 16:57:33 +0800

    --

--- testcase/ly_selenium.py
+++ testcase/ly_selenium.py
@@ -65,10 +65,12 @@ class ly(object):
         '''
         element = WebDriverWait(self.driver, timeout, 1).until(EC.presence_of_element_located(locator))
         return element
+
     def find_elements(self, locator, timeout=10):
         '''定位一组元素'''
         elements = WebDriverWait(self.driver, timeout, 1).until(EC.presence_of_all_elements_located(locator))
         return elements
+
     def click(self, locator):
         '''
         点击操作
@@ -78,6 +80,7 @@ class ly(object):
         '''
         element = self.find_element(locator)
         element.click()
+
     def send_keys(self, locator, text):
         '''
         发送文本，清空后输入
@@ -88,6 +91,7 @@ class ly(object):
         element = self.find_element(locator)
         element.clear()
         element.send_keys(text)
+
     def send_keys_botton(self, locator, text):
         '''
         发送文本，清空后输入
@@ -97,6 +101,7 @@ class ly(object):
         '''
         element = self.find_element(locator)
         element.send_keys(text)
+
     def is_text_in_element(self, locator, text, timeout=10):
         '''
         判断文本在元素里,没定位到元素返回False，定位到返回判断结果布尔值
@@ -109,6 +114,7 @@ class ly(object):
             return False
         else:
             return result
+
     def is_text_in_value(self, locator, value, timeout=10):
         '''
         判断元素的value值，没定位到元素返回false,定位到返回判断结果布尔值
@@ -121,45 +127,55 @@ class ly(object):
             return False
         else:
             return result
+
     def is_title(self, title, timeout=10):
         '''判断title完全等于'''
         result = WebDriverWait(self.driver, timeout, 1).until(EC.title_is(title))
         return result
+
     def is_title_contains(self, title, timeout=10):
         '''判断title包含'''
         result = WebDriverWait(self.driver, timeout, 1).until(EC.title_contains(title))
         return result
+
     def is_selected(self, locator, timeout=10):
         '''判断元素被选中，返回布尔值,'''
         result = WebDriverWait(self.driver, timeout, 1).until(EC.element_located_to_be_selected(locator))
         return result
+
     def is_selected_be(self, locator, selected=True, timeout=10):
         '''判断元素的状态，selected是期望的参数true/False
         返回布尔值'''
         result = WebDriverWait(self.driver, timeout, 1).until(EC.element_located_selection_state_to_be(locator, selected))
         return result
+
     def is_alert_present(self, timeout=10):
         '''判断页面是否有alert，
         有返回alert(注意这里是返回alert,不是True)
         没有返回False'''
         result = WebDriverWait(self.driver, timeout, 1).until(EC.alert_is_present())
         return result
+
     def is_visibility(self, locator, timeout=10):
         '''元素可见返回本身，不可见返回Fasle'''
         result = WebDriverWait(self.driver, timeout, 1).until(EC.visibility_of_element_located(locator))
         return result
+
     def is_invisibility(self, locator, timeout=10):
         '''元素可见返回本身，不可见返回True，没找到元素也返回True'''
         result = WebDriverWait(self.driver, timeout, 1).until(EC.invisibility_of_element_located(locator))
         return result
+
     def is_clickable(self, locator, timeout=10):
         '''元素可以点击is_enabled返回本身，不可点击返回Fasle'''
         result = WebDriverWait(self.driver, timeout, 1).until(EC.element_to_be_clickable(locator))
         return result
+
     def is_located(self, locator, timeout=10):
         '''判断元素有没被定位到（并不意味着可见），定位到返回element,没定位到返回False'''
         result = WebDriverWait(self.driver, timeout, 1).until(EC.presence_of_element_located(locator))
         return result
+
     def move_to_element(self, locator):
         '''
         鼠标悬停操作
@@ -169,6 +185,7 @@ class ly(object):
         '''
         element = self.find_element(locator)
         ActionChains(self.driver).move_to_element(element).perform()
+
     def back(self):
         """
         Back to old window.
